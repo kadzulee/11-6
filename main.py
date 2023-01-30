a19e4bbc88a2af71137615e75b9ccca57cd8c149
Ingmārs Meļķis <idmelkis@edu.riga.lv>
2023-01-30 09:54:49 +0200

    Karātavas

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
-print("whatever")
+import random
+
+vardi = ["viens", "divi", "tris", "cits"]
+
+while True:
+    vards = random.choice(vardi)
+    minetaisVards = list("_" for _ in vards)
+    dzivibas = 5
+
+    print(vards)
+
+    while dzivibas > 0 and not "".join(minetaisVards) == vards:
+        inp = input("Ievade: ")
+        inp = inp[0]
+        # TODO: garuma pārbaude
+
+        uzminets = False
+        for iii in range(0, len(vards)):
+            if vards[iii] == inp:
+                minetaisVards[iii] = inp
+                uzminets = True
+
+        if uzminets == False:
+            dzivibas -= 1
+
+        print(f"Dzivibas: {dzivibas}")
+        print("".join(minetaisVards))
+
+    if dzivibas == 0:
+        print("Jūs zaudējāt")
+    else:
+        print("Jūs uzvarējāt")
+
+    if not input("Vai turpināt? J/N: ")[0].lower() == "j":
+        break
