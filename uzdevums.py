83ecd37ab6899de3639a78b8d2be45d9a4f82d83
Ingmars Melkis <idmelkis@edu.riga.lv>
2023-02-13 09:03:39 +0200

    Pievienoju MD uzdevuma nos.

---
+++ uzdevums.py
@@ -0,0 +1,12 @@
+#Programma, kas ļauj ievadīt un saglabāt priekšmetu atzīmes
+
+#1. Jāļauj ievadīt priekšmetus (input)
+#2. Jāizvada ievadītie priekšmeti (print, for)
+#3. Jāļauj ievadīt priekšmetu atzīmes (input) -- 1 - 10, nv
+#4. Jāļauj lietotājam izvēlēties priekšmetu un izvadīt atzīmes saistītas ar šo priekšmetu (...)
+#5. Jānodrošina priekšmetu un atzīmju saglabāšana failā (json.dumps)
+#6. Jānodrošina priekšmetu un atzīmju ielāde no faila (json.load)
+#7. Jānodrošina priekšmetu un atzīmju dzēšana
+
+vardnica = { "prieksmeta_nos" : []}
+vardnica["prieksmets2"] = []
