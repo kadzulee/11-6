88e9953136781871a326f7e1aeeeaa4b2a8da8af
Ingmars Melkis <idmelkis@edu.riga.lv>
2023-03-20 08:45:20 +0200

    Papildināju klašu piemēru - pabeidzu pirkuma loģiku

--- klasupiemers-restorans/main.py
+++ klasupiemers-restorans/main.py
@@ -19,7 +19,7 @@ class Restorans:
             print("Kluda: nav pieejami viesmili")
         else:
             viesmilis._kaduApkalpo = True
-            return Pasutijums(klients, ediens, viesmilis)
+            return Pasutijums(klients, ediens, viesmilis, self._virtuve)
 class Darbinieks:
     _alga: float = 100.0
     def __init__(self) -> None:
@@ -40,12 +40,19 @@ class Viesmilis(Darbinieks):
         return super().IzvadamAlgu() + self._dzeramnauda
 class Sastavdala:
     _izmaksas: float = 0.0
+    _nosaukums: str
 class Recepte:
     _sastavdalas: 'list[Sastavdala]'
     _instrukcijas: 'list[str]'
     def __init__(self) -> None:
         self._sastavdalas = []
         self._instrukcijas = []
+    def AprekinamIzmaksas(self) -> float:
+        kopIzmaksas = 0.0
+        for sastavdala in self._sastavdalas:
+            kopIzmaksas += sastavdala._izmaksas
+        return kopIzmaksas
+
 class Ediens:
     _sastavdalas: 'list[Sastavdala]'
     def __init__(self) -> None:
@@ -61,32 +68,64 @@ class Pasutijums:
     _klients: Klients
     _receptes: 'list[Recepte]'
     _viesmilis: Viesmilis
-    def __init__(self, klients: Klients, receptes: 'list[Recepte]', viesmilis: Viesmilis) -> None:
+    _virtuve: 'Virtuve'
+    def __init__(self, klients: Klients, receptes: 'list[Recepte]', viesmilis: Viesmilis, virtuve: 'Virtuve') -> None:
         self._klients = klients
         self._receptes = receptes
         self._viesmilis = viesmilis
+        self._virtuve = virtuve
     def AprekinamIzmaksas(self) -> float:
         kopIzmaksas = 0.0
         for recepte in self._receptes:
-            kopIzmaksas += recepte._izmaksas
+            kopIzmaksas += recepte.AprekinamIzmaksas()
         return kopIzmaksas
+    def Izpildam(self):
+        izmaksas = self.AprekinamIzmaksas()
+        if self._klients._pieejamaNauda < izmaksas:
+            print("Klientam nepietiek naudas!")
+            return
+        else:
+            self._klients._pieejamaNauda -= izmaksas
+            return self._virtuve.Gatavot(self._receptes)
+
 class Virtuve:
-    _sastavdalas: 'list[Sastavdala]'
-    def Gatavot(pavars: Pavars, ediens: Ediens) -> None:
-        pass
+    _sastavdalas: 'dict[Sastavdala, int]'
+    def __init__(self) -> None:
+        self._sastavdalas = {}
+    def Gatavot(self, ediens: 'list[Recepte]') -> 'list[Ediens]':
+        # Te vajadzētu piesaistīt pavāru. Mēs to šajā versijā nedarīsim.
+        pagatavotaisEdiens = []
+        for recepte in ediens:
+            # Pārbaudam vai vispār pietiek sastāvdaļas
+            for sastavdala in recepte._sastavdalas:
+                if self._sastavdalas[sastavdala] < 1:
+                    print("Nepietiek sastāvdaļas!")
+            # Izmantojam sastāvdaļas
+            for sastavdala in recepte._sastavdalas:
+                self._sastavdalas[sastavdala] -= 1
+
+            pagEdiens = Ediens()
+            pagEdiens._sastavdalas = recepte._sastavdalas
+            pagatavotaisEdiens.append(pagEdiens)
+        return pagatavotaisEdiens
+
+udens = Sastavdala()
+udens._izmaksas = 0.2
+udens._nosaukums = "Ūdens"
+kartupeli = Sastavdala()
+kartupeli._izmaksas = 0.6
+kartupeli._nosaukums = "Kartupeļi"
+virtuve = Virtuve()
+virtuve._sastavdalas[udens] = 2
+virtuve._sastavdalas[kartupeli] = 3
 
 Janis = Pavars()
 Mikelis = Viesmilis()
-virtuve = Virtuve()
 restorans = Restorans(virtuve)
 restorans._darbinieki.append(Janis)
 restorans._darbinieki.append(Mikelis)
 Mikelis._dzeramnauda = 200
 
-udens = Sastavdala()
-udens._izmaksas = 0.2
-kartupeli = Sastavdala()
-kartupeli._izmaksas = 0.6
 recepte = Recepte()
 recepte._sastavdalas.append(udens)
 recepte._sastavdalas.append(kartupeli)
@@ -97,8 +136,7 @@ klients = Klients()
 klients._pieejamaNauda = 200.0
 
 pasutijums = restorans.PirktEdienu(klients, [recepte])
-print(Mikelis._kaduApkalpo)
-
-# TODO: Pabeigt pirkuma secību
-# Virtuvē jāpabeidz gatavot, jānomaina _kaduApkalpo uz False
-# Jāatgriež list[Ēdiens]
+ediens = pasutijums.Izpildam()
+print(f"Klients nopirka {len(ediens)} ēdienus. Klientam palika {klients._pieejamaNauda} naudiņas!")
+for sastavdala in restorans._virtuve._sastavdalas:
+    print(f"Palika sastavdala {sastavdala._nosaukums}: {restorans._virtuve._sastavdalas[sastavdala]}")
